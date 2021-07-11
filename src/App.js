caebc9165472b68e54f79682b3f8852a0acc150c
Ananya Gupta <ananyag2806@gmail.com>
2021-07-12 00:25:53 +0530

    Added TinderCards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import TinderCard from './TinderCards';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       {/* header */}
       <Header/>
       {/* tinder cards */}
+      <TinderCard/>
       {/* swipe buttons */}
     </div>
   );
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TinderCards.css'
+import TinderCard from 'react-tinder-card';
 
 function TinderCards() {
+
+    const [people, setPeople] = useState([
+        {
+            name: "Elon Musk",
+            url: "https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg"
+        },
+        {
+            name: "Jeff Bezoz",
+            url: "https://image.cnbcfm.com/api/v1/image/105510261-1591797492468preview.jpg?v=1620822045&w=630&h=354"
+        },
+        {
+            name: "Sandra Bullock",
+            url: "https://www.usmagazine.com/wp-content/uploads/2020/02/Sandra-Bullock-Plans-to-Move-%E2%80%98Down-the-Street%E2%80%99-From-Her-Kids-When-They-Start-College.jpg?w=1000&quality=86&strip=all"
+        },
+    ]);
+
+    const swiped = (direction, nameToDelete) =>{
+        console.log("removing" + nameToDelete);
+    };
+
+    const outOfFrame = (name) =>{
+        console.log(name + "left the screen");
+    };
+
     return (
-        <div>
-            
+        <div className="tinderCards">
+            <div className="tinderCardsContainer">
+
+                {people.map((person) => (
+                    <TinderCard
+                    className="swipe"
+                    key={person.name}
+                    preventSwipe={["up", "down"]}
+                    onSwipe={(dir) => swiped(dir, person.name)}
+                    onCardLeftScreen={() => outOfFrame(person.name)}
+                    >
+                        <div style={{backgroundImage: `url(${person.url})`}}
+                        className="card">
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+
+                ))}
+            </div>
         </div>
     )
 }

97232c87151e21fbc0758f8c64b8bfe057c5b48c
Ananya Gupta <ananyag2806@gmail.com>
2021-07-14 22:19:18 +0530

    Connected Frontend and Backend

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,19 +1,54 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Cards from './dbCards.js'
+import Cors from 'cors';
 
 
 //App config
 const app = express();
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://admin:I1I9Bz8JM3bMwi9y@cluster0.ekplo.mongodb.net/tinderdb?retryWrites=true&w=majority'
 
 //Middlewares
-
+app.use(express.json()); 
+app.use(Cors());
 
 //DB config
-
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
 
 //Api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCards = req.body;
+    
+    Cards.create(dbCards, (err, data) =>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    })
+
+})
+
+app.get('/tinder/cards', (req, res) => {
+    
+    Cards.find((err, data) =>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    })
+
+})
 
 //Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
--- tinder-frontend/src/TinderCards.js
+++ tinder-frontend/src/TinderCards.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './TinderCards.css'
 import TinderCard from 'react-tinder-card';
+import axios from './axios';
 
 function TinderCards() {
 
-    const [people, setPeople] = useState([
-        {
-            name: "Elon Musk",
-            url: "https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg"
-        },
-        {
-            name: "Jeff Bezoz",
-            url: "https://image.cnbcfm.com/api/v1/image/105510261-1591797492468preview.jpg?v=1620822045&w=630&h=354"
-        },
-        {
-            name: "Sandra Bullock",
-            url: "https://www.usmagazine.com/wp-content/uploads/2020/02/Sandra-Bullock-Plans-to-Move-%E2%80%98Down-the-Street%E2%80%99-From-Her-Kids-When-They-Start-College.jpg?w=1000&quality=86&strip=all"
-        },
-    ]);
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const req = await axios.get("/tinder/cards");
+
+            setPeople(req.data);
+        }
+
+        fetchData();
+    }, [])
+
+    console.log(people);
 
     const swiped = (direction, nameToDelete) =>{
         console.log("removing" + nameToDelete);
@@ -39,7 +39,7 @@ function TinderCards() {
                     onSwipe={(dir) => swiped(dir, person.name)}
                     onCardLeftScreen={() => outOfFrame(person.name)}
                     >
-                        <div style={{backgroundImage: `url(${person.url})`}}
+                        <div style={{backgroundImage: `url(${person.imgUrl})`}}
                         className="card">
                             <h3>{person.name}</h3>
                         </div>
